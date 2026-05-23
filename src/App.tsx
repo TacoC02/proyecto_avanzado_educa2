394dddc826c9e8dcbc759f7186de86fb38e41a93
Juanchis2603 <raix1034@gmail.com>
2026-05-23 10:58:06 -0400

    arreglo de error que no se veia la carta devido a las vistas

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import Mazo from './Componentes/VistaMazo'
 import CrearCartaPage from './pages/CrearCartaPage'
 import EditarCartaPage from './pages/EditarCartaPage'
+import CardDetailPage from './pages/CardDetailPage'
 import BattlePage from './pages/BattlePage'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/card" element={<Mazo />} />
         <Route path="/card/create" element={<CrearCartaPage />} />
         <Route path="/card/edit/:id" element={<EditarCartaPage />} />
+        <Route path="/card/:id" element={<CardDetailPage />} />
         <Route path="/card/battle" element={<BattlePage />} />
         <Route path="*" element={<Navigate to="/card" replace />} />
       </Routes>
